7ca72f24007127ed456daae8df216cbb35e1f6e3
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2020-12-26 13:12:58 +0100

    guessedWords component tested and done

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -41,5 +41,27 @@ describe('if there are no words guessed', ()=>{
 })
 
 describe('if there are words guessed', ()=>{
+    let wrapper;
+    const guessedWords = [
+        {guessedWord:'train', letterMatchCount:3},
+        {guessedWord:'agile', letterMatchCount:1},
+        {guessedWord:'party', letterMatchCount:5},
+
+    ];
+    beforeEach(()=>{
+        wrapper= setup({guessedWords});
+    })
+    test('renders without errors',()=>{
+        const component = findByTestAttr(wrapper,'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+    test('renders guessed words section',()=>{
+        const guessedWordNode = findByTestAttr(wrapper,'guessed-words');
+        expect(guessedWordNode.length).toBe(1);
+    });
+    test('correct numbers of guessed words',()=>{
+        const guessedWordNodes = findByTestAttr(wrapper,'guessed-word');
+        expect(guessedWordNodes.length).toBe(guessedWords.length);
+    });
     
 })
