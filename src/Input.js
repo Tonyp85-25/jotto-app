eec926f9a03c9284477153bc6728541ef7ba9302
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2020-12-29 17:47:08 +0100

    set up input component and storeFactory

---
+++ src/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const Input = ()=>{
+    return <div/>
+}
+
+const mapsStateToProps = (state) => {
+    return {};
+}
+
+export default connect(mapsStateToProps)(Input)
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,6 @@
-import { checkPropTypes } from "prop-types"
-
+import { checkPropTypes } from "prop-types";
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers';
 /**
  * Return node(s) with the given data-test attribute
  * @param {ShallowWrapper} wrapper 
@@ -17,4 +18,15 @@ export const checkProps = (component,conformingProps) =>{
         'prop',
         component.name);
     expect(propError).toBeUndefined();
+}
+
+/**
+ * Creates a testing store with imported reducers, middlewares, and initial state
+ * @global rootReducer
+ * @param {object} initialState - Initial state for store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ */
+export const storeFactory = (initialState) =>{
+    return createStore(rootReducer,initialState);
 }
