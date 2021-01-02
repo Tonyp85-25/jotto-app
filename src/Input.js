d023ba94f1735979c3ca93ead8c1a490b8bda497
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2021-01-02 18:44:57 +0100

    word not guessed coded and tested

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {connect} from 'react-redux';
 
-const Input = ()=>{
-    return <div/>
+class Input extends Component{
+    render(){
+      const contents=  this.props.success? null: (<form>
+          <input type="text" placeholder="enter guess" data-test='input-box'/>
+          <button data-test="submit-button" type="submit">Submit</button>
+      </form>);
+        return <div data-test='component-input'>
+            {contents}
+        </div>
+    }
+   
 }
 
-const mapsStateToProps = (state) => {
-    return {};
+const mapStateToProps = ({success}) => {
+    return { success};
 }
 
-export default connect(mapsStateToProps)(Input)
+export default connect(mapStateToProps)(Input)
