f2581d180586795f8d3bbdf8bf1dbbb5ec43a0c4
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2021-04-10 20:24:10 +0200

    app component updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Congrats from './Congrats'
 class App extends Component {
   render(){
     return (
-      <div className="App">
+      <div className="App" data-test='component-app' >
        <h1>Jotto</h1>
        <Congrats success={false} />
        <GuessedWords guessedWords={[]}/>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-
+import {shallow} from 'enzyme'
+import {findByTestAttr} from '../test/testUtils'
 import App from './App';
 
-
-
-test('renders learn react link', () => {
-  
+/**
+ * Setup function for App component
+ * @returns {ShallowWrapper}
+ */
+const setup = ()=>{
+    return shallow(<App/>)
+}
+test('renders without errors', () => {
+  const wrapper= setup()
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent).toHaveLength(1)
 });
