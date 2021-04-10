59748f391cab076ceb5bc4ce002d033dd5094e62
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2021-04-10 19:03:28 +0200

    index component s almost ready and getLettermatchCount helper function added

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Input = ({secretWord})=>{
+const Input = ({success,secretWord})=>{
 
     const [currentGuess, setCurrentGuess] = React.useState('')
+    if(success){
+        return <div data-test='input'/>
+    }
     return <div data-test='input'>
         <form className="form-inline">
             <input data-test='input-box' type='text' placeholder="enter guess" value={currentGuess} onChange={(event)=> setCurrentGuess(event.target.value)}/>
-            <button onClick={event=>{event.preventDefault()}} data-test='submit-button'>Submit</button>
+            <button onClick={event=>{event.preventDefault(); setCurrentGuess('')}} data-test='submit-button'>Submit</button>
         </form>
     </div>
 }
--- src/Input.test.js
+++ src/Input.test.js
@@ -4,16 +4,50 @@ import {shallow} from 'enzyme';
 import Input from './Input'
 import { checkProps, findByTestAttr } from '../test/testUtils';
 
-const setup = (props={})=>{
+const setup = (success=false,secretWord='party')=>{
    // const setupProps({...props})
-    return shallow(<Input {...props}/>)
+    return shallow(<Input success={success} secretWord={secretWord}/>)
 }
+describe('render',()=>{
+    describe('success is true',()=>{
+        let wrapper
+        beforeEach(()=>{
+            wrapper =setup(true)
+        })
+        test('renders without errors',()=>{
+            const input= findByTestAttr(wrapper,'input')
+            expect(input.length).toBe(1)
+        })
+        test('input box does not show',()=>{
+            const inputBox = findByTestAttr(wrapper,'input-box')
+            expect(inputBox.exists()).toBe(false)
+        })
+        test('submit-button does not show',()=>{
+            const submitBtn =findByTestAttr(wrapper,'submit-button')
+            expect(submitBtn.exists()).toBe(false)
+        })
 
-test('renders without errors',()=>{
-    const wrapper =setup({secretWord:''})
-   const input= findByTestAttr(wrapper,'input')
-   expect(input.length).toBe(1)
+    })
+    describe('success is false',()=>{
+        let wrapper
+        beforeEach(()=>{
+            wrapper =setup()
+        })
+        test('input renders without errors',()=>{
+            const input= findByTestAttr(wrapper,'input')
+            expect(input.length).toBe(1)
+        })
+        test('input box is visible',()=>{
+            const inputBox = findByTestAttr(wrapper,'input-box')
+            expect(inputBox.exists()).toBe(true)
+        })
+        test('submit-button is visible',()=>{
+            const submitBtn =findByTestAttr(wrapper,'submit-button')
+            expect(submitBtn.exists()).toBe(true)
+        })
+    })
 })
+
 
 test('does not throw warning with expected props',()=>{
     const expectedProps = {secretWord:'bla'}
@@ -22,15 +56,27 @@ test('does not throw warning with expected props',()=>{
 
 // don't destructure imports in order to use mock functions! (useState can be mocked only if used as React.useState)
 describe('state controlled input field',()=>{
+    let wrapper
+    let mockSetCurrentGuess = jest.fn()
+    beforeEach(()=>{
+        mockSetCurrentGuess.mockClear()
+        React.useState = jest.fn(()=>["", mockSetCurrentGuess])
+        wrapper= setup()
+    })
 
     test('state update with value of input box upon change',()=>{
-        const mockSetCurrentGuess = jest.fn()
+
         React.useState = jest.fn(()=>['',mockSetCurrentGuess])
-        const wrapper= setup({secretWord:''})
         const inputBox= findByTestAttr(wrapper,'input-box')
         const mockEvent = {target:{value:'train'}}
         inputBox.simulate('change',mockEvent)
         expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
+    })
+
+    test('field is cleared upon submit button click',()=>{
+        const submitBtn =findByTestAttr(wrapper,'submit-button')
+        submitBtn.simulate('click',{preventDefault(){}})
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith("")
     })
 
 })
