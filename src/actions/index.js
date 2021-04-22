297cff482c23b96aec774a09ec3bf41ba1ba0170
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2021-04-22 23:02:58 +0200

    setup guessedWords piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import {getLetterMatchCount} from '../helpers'
 
 export const actionTypes ={
     CORRECT_GUESS : 'CORRECT_GUESS',
+    GUESS_WORD:'GUESS_WORD'
 }
 /**
  * @function correctGuess
@@ -24,6 +26,14 @@ export const getSecretWord = ()=>{
  */
 export const guessWord  =(guessedWord)=>{
     return function(dispatch,getState){
-
+        const secretWord = getState().secretWord
+        const letterMatchCount= getLetterMatchCount(guessedWord,secretWord)
+        dispatch({
+            type:actionTypes.GUESS_WORD,
+            payload: {guessedWord,letterMatchCount}
+        })
+        if(guessedWord === secretWord){
+            dispatch({type:actionTypes.CORRECT_GUESS,})
+        }
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 import success from './successReducer'
-import guessedWord from './guessedWordsReducer'
+import guessedWords from './guessedWordsReducer'
+import secretWord from './secretWordReducer'
 
 export default combineReducers({
     success,
-    guessedWord
+    guessedWords,
+    secretWord
 })
 
